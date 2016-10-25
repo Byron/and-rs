40eef50b564274fde3ac19e62b0535e28642510a
Sebastian Thiel <byronimo@gmail.com>
2016-10-25 08:01:53 +0200

    chore(travis): setup android testing
    See which tools are available, except for the emulator
    as it might just slow down initial node setup.
    
    Let's go step by step to see what we need.

--- bin/tests.sh
+++ bin/tests.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+function need_cmd() {
+  if ! command -v $1 >/dev/null 2>&1; then
+    echo "Need command to be available: $1" 1>&2
+    return 2
+  fi
+}
+
+for cmd in android keytool aapt javac javadoc dx jarsigner zipalign; do
+  need_cmd $cmd || exit $?
+done
+
 AND=${1:?First argument must be the 'and' program path to use for testing}
 echo "TBD with $AND"
 exit 3
