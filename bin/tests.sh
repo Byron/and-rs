86efe2f864b3d23e74795cadbfb512e03c9498a4
Sebastian Thiel <byronimo@gmail.com>
2016-10-26 08:15:04 +0200

    feat(tests): basic test system
    Allow to declare expecations and run a function to verify it,
    collecting all output in the process.

--- bin/tests.sh
+++ bin/tests.sh
@@ -13,6 +13,29 @@ if [[ -n $TRAVIS ]]; then
   export PATH=$build_tools:$PATH
 fi
 
+function it () {
+  local description=${1:?Need description text}
+  local test_function=${2:?Need function to execute}
+  local result
+  echo -n "$description"
+  local result
+  result=`eval $test_function 2>&1`
+  if [ $? = 0 ]; then
+    echo " - OK" 1>&2
+  else
+    echo " - failed" 1>&2
+    echo $result 1>&2
+    exit 2
+  fi
+}
+
 AND=${1:?First argument must be the 'and' program path to use for testing}
-echo "TBD with $AND"
-exit 3
+
+function test_it() {
+  echo "this works"
+  echo "this works on stderr" 1>&2
+  return 0
+}
+
+it 'can execute simple tests' test_it
+# it 'can create scaffolding for the most basic android application' test_create
