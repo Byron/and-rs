09d891e28a4c7e9948c9f571988a32167d7199ff
Sebastian Thiel <byronimo@gmail.com>
2016-10-28 17:49:51 +0200

    imp(spec): first actual spec

--- spec/acceptance_spec.cr
+++ spec/acceptance_spec.cr
@@ -1,9 +1,29 @@
 require "spec"
+require "io"
+
+def run_with(args)
+  ->(more_args : String) {
+    output = MemoryIO.new()
+    error = MemoryIO.new()
+    {
+      result: Process.run(
+        command: ENV["EXECUTABLE"], 
+        shell: false,
+        args: "#{args} #{more_args}".split(' '),
+        output: output, error: error, input: false),
+      output: output,
+      error: error
+    }
+  }
+end
 
 describe "`and` program" do
-  # TODO: Write tests
-
-  it "works" do
-    false.should eq(true)
+  describe "subcommands" do
+    describe "new" do
+      run = run_with "new"
+      it "does not accept non-ascii characters and dashes as project name" do
+        (run.call "hello-world$!123")[:result].exit_status.should eq 3
+      end
+    end
   end
 end
