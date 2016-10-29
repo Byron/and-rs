c8f65e2e1c2d114096a41b5bf37b558c143632ae
Sebastian Thiel <byronimo@gmail.com>
2016-10-29 10:45:18 +0200

    imp(lib): creating my first directory :)

--- src/lib/src/lib.rs
+++ src/lib/src/lib.rs
@@ -1,9 +1,11 @@
 #[macro_use]
 extern crate quick_error;
 
+use quick_error::ResultExt;
 use std::error::Error as ErrorTrait;
 use std::path::{Path, PathBuf};
 use std::io;
+use std::fs::create_dir_all;
 
 quick_error!{
     #[derive(Debug)]
@@ -27,6 +29,19 @@ pub struct Context {
     pub package_path: String
 }
 
+fn dotted_package_name_to_package_path(name: &str) -> String {
+    Path::new("src").join(name.replace(".", "/")).to_string_lossy().into_owned()
+}
+
 pub fn generate_application_scaffolding(ctx: Context) -> Result<(), Error> {
+    let app_path = |path: &str| Path::new(&ctx.application_name).join(path);
+    let package_dir = app_path(&dotted_package_name_to_package_path(&ctx.package_path));
+    try!(create_dir_all(&package_dir).context(package_dir.as_path()));
     Ok(())
+}
+
+
+#[test]
+fn test_dotted_package_name_to_package_path() {
+    assert_eq!(dotted_package_name_to_package_path("hello.wonderful.world"), "src/hello/wonderful/world");
 }
