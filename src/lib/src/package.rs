b69b4de2e1639eb2e63283a24aa62f9f4ce0928d
Sebastian Thiel <byronimo@gmail.com>
2016-11-05 17:37:24 +0100

    chore(travis): check package tools on linux

--- src/lib/src/package.rs
+++ src/lib/src/package.rs
@@ -1,6 +1,11 @@
 use std::path::Path;
-use super::{BatchExecutionError, Context};
+use super::{BatchExecutionError, Context, find_android_executable, find_file_in_path};
 
 pub fn package_application(at: &Path, ctx: &Context) -> Result<(), BatchExecutionError> {
+    let (dx_path, _) = try!(find_android_executable("dx"));
+    let (aapt_path, _) = try!(find_android_executable("aapt"));
+    let (zipalign_path, _) = try!(find_android_executable("zipalign"));
+    let jarsigner_path = try!(find_file_in_path("jarsigner"));
+
     Ok(())
 }
