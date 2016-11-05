7f6407b807d661c31b17565fd76ea5dc7cd5974e
Sebastian Thiel <byronimo@gmail.com>
2016-11-05 19:09:20 +0100

    imp(lib): try to create debugstore - fail
    The error handling chaining should be done differently

--- src/lib/src/package.rs
+++ src/lib/src/package.rs
@@ -2,20 +2,29 @@ use std::path::{PathBuf, Path};
 use super::{execute_program_verbosely, BatchExecutionError, Context, find_android_executable,
             find_file_in_path, android_platform_jar_path, get_env_as_path, FindError};
 
-fn verified_android_keystore_path() -> Result<PathBuf, FindError> {
-    get_env_as_path("HOME").and_then(|home| {
-        const ANDROID_KEYSTORE_NAME: &'static str = "debug.keystore";
-        let dir = home.join(".android");
-        let keystore = dir.join(ANDROID_KEYSTORE_NAME);
-        if keystore.is_file() {
-            Ok(keystore)
-        } else {
-            Err(FindError::NotFound {
-                dir: dir,
-                name: ANDROID_KEYSTORE_NAME.to_owned(),
+fn fetch_or_create_android_keystore() -> Result<PathBuf, BatchExecutionError> {
+    const ANDROID_KEYSTORE_NAME: &'static str = "debug.keystore";
+    let home = try!(get_env_as_path("HOME"));
+    let dir = home.join(".android");
+    let keystore = dir.join(ANDROID_KEYSTORE_NAME);
+    if keystore.is_file() {
+        Ok(keystore)
+    } else {
+        find_file_in_path("keytool")
+            .map_err(BatchExecutionError::from)
+            .and_then(|keytool_path| {
+                execute_program_verbosely(Path::new("."), &keytool_path, &[])
+                    .map(|_| keystore)
+                    .map_err(|_| {
+                        FindError::NotFound {
+                                name: ANDROID_KEYSTORE_NAME.to_owned(),
+                                dir: dir,
+                            }
+                            .into()
+                    })
             })
-        }
-    })
+    }
+
 }
 
 pub fn package_application(at: &Path, ctx: &Context) -> Result<(), BatchExecutionError> {
@@ -23,7 +32,7 @@ pub fn package_application(at: &Path, ctx: &Context) -> Result<(), BatchExecutio
     let (aapt_path, _) = try!(find_android_executable("aapt"));
     let (zipalign_path, _) = try!(find_android_executable("zipalign"));
     let jarsigner_path = try!(find_file_in_path("jarsigner"));
-    let debug_keystore_path = try!(verified_android_keystore_path());
+    let debug_keystore_path = try!(fetch_or_create_android_keystore());
 
     try!(execute_program_verbosely(at,
                                    &dx_path,
